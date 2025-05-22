18348989b60218df0f45801dd2c2bfa75cc89574
Sergio Duran <538ser@gmail.com>
2025-05-22 16:28:10 -0600

    Fix paths for CSS and JS files; enable dark mode toggle functionality

--- javascipt/jav.js
+++ javascipt/jav.js
@@ -1,4 +1,10 @@
 
+
+document.addEventListener("DOMContentLoaded", () => {
+
   let mode = document.getElementById("theme-toggle");
 
- 
+  mode.addEventListener('click', () => {
+    document.body.classList.toggle("dark-mode");
+  });
+});
