3ae87e8577a471c7feca17df4adbf30c475c53b8
nikolaiyanuzov <NSYanuzov23@codingburgas.bg>
2025-06-01 15:55:36 +0300

    Add page for the departments information

--- javascipt/departments.js
+++ javascipt/departments.js
@@ -1,9 +1,10 @@
-const listTemplate = document.querySelector("[user-template]");
-const container = document.querySelector("[data-user-container]");
-const dataInput = document.querySelector("[data-search]");
-
-let store = [];
-
+function search()
+{
+  const listTemplate = document.querySelector("[user-template]");
+  const container = document.querySelector("[data-user-container]");
+  const dataInput = document.querySelector("[data-search]");
+  
+  let store = [];
 dataInput.addEventListener("input", (e) => {
   const value = e.target.value.toLowerCase();
   store.forEach(user => {
@@ -30,9 +31,19 @@ fetch('../data/department-info.json')
   .then(data => {
     store = data.map(department => {
       const li = listTemplate.content.cloneNode(true).querySelector("li");
-      li.textContent = department[1];
+      li.innerHTML = `<a href="./departmentSearch.html" class="item-link" link>${department[1]}</a>`;
+
+      const link = li.querySelector('[link]');
+       link.addEventListener('click', (e) => {
+          e.preventDefault()
+           localStorage.setItem('id', department[0]);
+  window.location.href = "../html/departmentSearch.html";
+       })
+
+
       container.append(li);
       return { name: department[1], element: li };
     });
   })
   .catch(error => console.error("Error loading file:", error));
+}
